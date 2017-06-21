f67cb9f0dc4d4918e0c8337a810aed388087e7be
bailey <bkdmarshall@hotmail.com>
2017-06-21 10:14:11 -0400

    exercise 5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Bailey DAntoni'
+my_age = 23 # current age
+my_height = 5 # feet
+my_weight = 140 # lbs 
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about Bailey DAntoni."
+puts "She's 5 feet tall."
+puts "She's 140 pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got Brown eyes and Brown hair."
+puts "Her teeth are usually White depending on coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add 23, 5, and 140 I get 168."
