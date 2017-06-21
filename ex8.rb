6938ced479c92bc8fd5bccc5b34417736f03620d
bailey <bkdmarshall@hotmail.com>
2017-06-21 14:21:40 -0400

    one through ten

--- ex8.rb
+++ ex8.rb
@@ -1,4 +1,4 @@
-p formatter = "%{first} %{second} %{third} %{fourth}"
+formatter = "%{first} %{second} %{third} %{fourth}"
 
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
